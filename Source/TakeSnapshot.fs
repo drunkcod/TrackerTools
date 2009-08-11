9730431b61ba249457b7530e23c127858e245e5c
Torbjorn Gyllebring <i.am@drunkencoder.com>
2009-08-11 23:44:50 +0200

    Unhelpfull but existing argument parsning to check function TakeSnapshot or CreateStoryCard

--- Source/TakeSnapshot.fs
+++ Source/TakeSnapshot.fs
@@ -10,16 +10,25 @@ module Program =
         use reader = new StreamReader(stream)
         let serializer = XmlSerializer(typeof<'a>)
         serializer.Deserialize(reader) :?> 'a
+
+    let TakeSnapshot (tracker:Tracker) =
+        let SaveSnapshot (stream:Stream) = 
+            use reader = new StreamReader(stream)
+            let targetPath = Path.Combine(Configuration.OutputDirectory, DateTime.Today.ToString("yyyy-MM-dd") + ".xml")
+            File.WriteAllText(targetPath, reader.ReadToEnd())
+        tracker.GetStories Configuration.ProjectId SaveSnapshot
     
-    let SaveSnapshot (stream:Stream) = 
-        use reader = new StreamReader(stream)
-        let targetPath = Path.Combine(Configuration.OutputDirectory, DateTime.Today.ToString("yyyy-MM-dd") + ".xml")
-        File.WriteAllText(targetPath, reader.ReadToEnd())
-       
-    [<EntryPoint>]
-    let main array =
-        let tracker = Tracker(Configuration.ApiToken)
-        let story = tracker.GetStory Configuration.ProjectId 989680 FromXml<TrackerStory>
+    let CreateStoryCard storyId (tracker:Tracker) =
+        let story = tracker.GetStory Configuration.ProjectId storyId FromXml<TrackerStory>
         File.WriteAllText("Output.html", StoryTemplate.Replace("$(Name)", story.Name).Replace("$(Description)", story.Description.Replace("\n", "<br>")))
-        //tracker.GetStories Configuration.ProjectId SaveSnapshot
+    
+    let ShowHelp x = ()
+
+    [<EntryPoint>]
+    let main args =
+        Tracker(Configuration.ApiToken)
+        |> match args.[0] with
+        | "TakeSnaphot" -> TakeSnapshot
+        | "CreateStoryCard" -> CreateStoryCard (Int32.Parse(args.[1]))
+        | _ -> ShowHelp
         0
