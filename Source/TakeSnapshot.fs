80f86ca5d75a73785201e3e0b4dc558e493e8e47
Torbjorn Gyllebring <i.am@drunkencoder.com>
2009-08-12 16:17:59 +0200

    Save snapshot to storyid.html.

--- Source/TakeSnapshot.fs
+++ Source/TakeSnapshot.fs
@@ -18,9 +18,12 @@ module Program =
             File.WriteAllText(targetPath, reader.ReadToEnd())
         tracker.GetStories Configuration.ProjectId SaveSnapshot
     
-    let CreateStoryCard storyId (tracker:Tracker) =
+    let CreateStoryCard (storyId:int) (tracker:Tracker) =
+        let FormatDescription (story:TrackerStory) = story.Description.Replace("\n", "<br>")
+        let ProcessTemplate (story:TrackerStory) =
+            StoryTemplate.Replace("$(Name)", story.Name).Replace("$(Description)",FormatDescription story )
         let story = tracker.GetStory Configuration.ProjectId storyId FromXml<TrackerStory>
-        File.WriteAllText("Output.html", StoryTemplate.Replace("$(Name)", story.Name).Replace("$(Description)", story.Description.Replace("\n", "<br>")))
+        File.WriteAllText(String.Format("{0}.html", storyId), ProcessTemplate story)
     
     let ShowHelp x = ()
 
@@ -28,7 +31,7 @@ module Program =
     let main args =
         Tracker(Configuration.ApiToken)
         |> match args.[0] with
-        | "TakeSnaphot" -> TakeSnapshot
-        | "CreateStoryCard" -> CreateStoryCard (Int32.Parse(args.[1]))
-        | _ -> ShowHelp
+            | "TakeSnaphot" -> TakeSnapshot
+            | "CreateStoryCard" -> CreateStoryCard (Int32.Parse(args.[1]))
+            | _ -> ShowHelp
         0
