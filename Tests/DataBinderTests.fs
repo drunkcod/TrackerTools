b2300839561381d2ffc9b63c9a7a18944f418cd2
Torbjorn Gyllebring <i.am@drunkencoder.com>
2009-08-21 14:52:33 +0200

    DataBinderTests started.

---
+++ Tests/DataBinderTests.fs
@@ -0,0 +1,19 @@
+﻿namespace TrackerTools
+open System
+open NUnit.Framework
+
+module DataBinder =
+    let Bind(format:string, item) = 
+        let result = ref format
+        item.GetType().GetProperties()
+        |> Seq.iter (fun prop -> result := (!result).Replace(String.Format("$({0})", prop.Name), prop.GetValue(item, null).ToString()))
+        !result
+
+module DataBinderTests =
+    type Item() =
+        let mutable someProperty = String.Empty
+        member this.SomeProperty with get() = someProperty and set(value) = someProperty <- value
+ 
+    [<Test>]
+    let should_support_binding_to_public_properties() =
+        Assert.That(DataBinder.Bind("$(SomeProperty)", Item(SomeProperty = "42")), Is.EqualTo("42"))
--- Tests/GetIterationTests.fs
+++ Tests/GetIterationTests.fs
@@ -9,7 +9,7 @@ module When_parsing_GetIterations_sample_response =
     let number_of_iterations_should_match() = Assert.That(result.Items.Count, Is.EqualTo(5))
     [<Test>]
     let iterations_should_contain_stories() = 
-        Assert.That(result.Items.[0].Stories, Is.EqualTo(2))
+        Assert.That(result.Items.[0].Stories.Count, Is.EqualTo(2))
     [<Test>]
     let first_iteration_first_story_should_be_a_feature() = 
         Assert.That(result.Items.[0].Stories.[0].StoryType, Is.EqualTo("feature"))
