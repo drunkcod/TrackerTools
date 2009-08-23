bc35e79034ede6197ec5e708efb6723544e6164e
Torbjorn Gyllebring <i.am@drunkencoder.com>
2009-08-23 16:49:59 +0200

    TrackerTask made data binding friendly.

--- Source/TrackerTask.fs
+++ Source/TrackerTask.fs
@@ -4,8 +4,14 @@ type IgnoreIf = System.ComponentModel.DefaultValueAttribute
 
 [<XmlRoot("task")>]
 type TrackerTask() =
-    [<DefaultValue;XmlElement("id");IgnoreIf(0)>] val mutable Id : int
-    [<DefaultValue;XmlElement("description")>] val mutable Description : string
-    [<DefaultValue;XmlElement("position");IgnoreIf(0)>] val mutable Position : int
-    [<DefaultValue;XmlElement("complete");IgnoreIf(false)>] val mutable Complete : bool
-    [<DefaultValue;XmlElement("created_at")>] val mutable CreatedAt : string
+    [<DefaultValue>] val mutable private id : int;
+    [<DefaultValue>] val mutable private description : string
+    [<DefaultValue>] val mutable private position : int
+    [<DefaultValue>] val mutable private complete : bool
+    [<DefaultValue>] val mutable private createdAt : string
+    
+    [<XmlElement("id");IgnoreIf(0)>] member this.Id with get() = this.id and set(value) = this.id <- value
+    [<XmlElement("description")>] member this.Description with get() = this.description and set(value) = this.description <- value
+    [<XmlElement("position");IgnoreIf(0)>] member this.Position with get() = this.position and set(value) = this.position <- value
+    [<XmlElement("complete");IgnoreIf(false)>] member this.Complete with get() = this.complete and set(value) = this.complete <- value
+    [<XmlElement("created_at")>] member this.CreatedAt with get() = this.createdAt and set(value) = this.createdAt <- value
