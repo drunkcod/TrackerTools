ac750049ce8b362291f2ef9176a8c0fbc5e4af20
Torbjorn Gyllebring <i.am@drunkencoder.com>
2009-09-02 00:04:45 +0200

    First tests in CreateProjectScenario, done.

---
+++ Server/Controllers/ProjectsController.cs
@@ -0,0 +1,35 @@
+﻿namespace Server.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using TrackerTools.Web;
+
+    class CreatedResult : ActionResult
+    {
+        readonly string location;
+        readonly ActionResult innerResult;
+
+        public CreatedResult(string location, ActionResult innerResult) 
+        {
+            this.location = location;
+            this.innerResult = innerResult; 
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+            response.StatusCode = 201;
+            response.AddHeader("Location", context.HttpContext.Request.Url + location);
+            innerResult.ExecuteResult(context);
+        }
+    }
+
+    public class ProjectsController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return new CreatedResult("1", new XmlResult("Hello World!"));
+        }
+    }
+}
---
+++ Source/Web/CreatedResult.cs
@@ -0,0 +1,24 @@
+﻿namespace TrackerTools.Web
+{
+    using System.Web.Mvc;
+
+    public class CreatedResult : ActionResult
+    {
+        readonly string location;
+        readonly ActionResult innerResult;
+
+        public CreatedResult(string location, ActionResult innerResult)
+        {
+            this.location = location;
+            this.innerResult = innerResult;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+            response.StatusCode = 201;
+            response.AddHeader("Location", context.HttpContext.Request.Url + location);
+            innerResult.ExecuteResult(context);
+        }
+    }
+}
