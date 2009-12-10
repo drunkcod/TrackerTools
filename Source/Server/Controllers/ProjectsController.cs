c0ecb19629cf18747fe5a436d7cc3c4d2caf965a
unknown <drunkcod@.(none)>
2009-12-10 22:24:14 +0100

    Content/Logic separation of Server.

---
+++ Source/Server/Controllers/ProjectsController.cs
@@ -0,0 +1,15 @@
+﻿namespace TrackerTools.Server.Controllers
+{
+    using TrackerTools;
+    using TrackerTools.Web;
+    using System.Web.Mvc;
+
+    public class ProjectsController : Controller
+    {
+        [XmlInput("project"), AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Index(TrackerProject project)
+        {
+            return new CreatedResult("1", new XmlResult(project));
+        }
+    }
+}
---
+++ Source/Server/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using TrackerTools.Web;
+
+namespace TrackerTools.Server
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode,
+    // visit http://go.microsoft.com/?LinkId=9394801
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterRoutes(RouteCollection routes) {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.Add(new Route("api/{*params}", new HttpHandlerRoute(() => new RestHandler())));
+
+            routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Tasks", action = "Index", id = "" }  // Parameter defaults
+                );
+        }
+
+        protected void Application_Start() {
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
