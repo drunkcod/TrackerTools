31de5bd1873f6dc96b69d8df828f8e4be798ad1f
Torbjorn Gyllebring <i.am@drunkencoder.com>
2009-12-11 20:45:48 +0100

    PostAttribute added.

---
+++ Source/Web/PostAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+
+namespace TrackerTools.Web
+{
+    public class PostAttribute : ActionMethodSelectorAttribute
+    {
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo) {
+            return controllerContext.HttpContext.Request.HttpMethod == "POST";
+        }
+    }
+}
