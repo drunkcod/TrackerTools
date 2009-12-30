bbd1d4c3f6f383bff636635c276f24eb12ab384b
Torbjorn Gyllebring <i.am@drunkencoder.com>
2009-12-30 20:32:34 +0100

    HasField uses case insensitive match.

--- Source/Web/BasicControllerFactory.cs
+++ Source/Web/BasicControllerFactory.cs
@@ -6,23 +6,25 @@ using Xlnt.Stuff;
 
 namespace TrackerTools.Web
 {
-    public class BasicControllerFactory : DefaultControllerFactory
+    public class BasicControllerFactory : IControllerFactory
     {
         public override IController CreateController(RequestContext requestContext, string controllerName){
             var wantedName = controllerName + "Controller";
-            foreach (var item in Controllers)
-                if (item.Name == wantedName)
+            foreach(var item in Controllers)
+                if(item.Name == wantedName)
                     return item.ConstructAs<IController>();
             throw new NotSupportedException();
         }
 
         IEnumerable<Type> Controllers {
             get {
-                foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-                    foreach (var type in assembly.GetTypes())
-                        if (typeof(IController).IsAssignableFrom(type))
+                foreach(var assembly in AppDomain.CurrentDomain.GetAssemblies())
+                    foreach(var type in assembly.GetTypes())
+                        if(typeof(IController).IsAssignableFrom(type))
                             yield return type;
             }
         }
+
+        public void ReleaseController(IController controller){}
     }
 }
