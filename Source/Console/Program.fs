38680756fa07dd25a13bbf50622a82e535fc2ee4
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-03-15 14:42:47 +0100

    Doh. Moved the console app without doing the lookup into the right assembly.

--- Source/Console/Program.fs
+++ Source/Console/Program.fs
@@ -15,37 +15,43 @@ type ConsoleApplication(configuration:TrackerToolsConfiguration) =
         | x when x = typeof<TrackerApi> -> box tracker
         | x when x = typeof<TrackerToolsConfiguration> -> box configuration
         | _ -> ifMissing(argument)
-
-    member this.FindCommand bind name =
-        let command =
-            Assembly.GetExecutingAssembly().GetTypes()
+        
+    member private this.Commands =         
+            typeof<ITrackerToolsCommand>.Assembly.GetTypes()
             |> Seq.filter (fun x -> typeof<ITrackerToolsCommand>.IsAssignableFrom(x))
-            |> Seq.tryFind (fun x ->
-                let commandNames = x.GetCustomAttributes(typeof<CommandNameAttribute>, false)
-                commandNames.Length > 0 && (commandNames.[0] :?> CommandNameAttribute).Name = name)
+            |> Seq.choose (fun x ->
+                let names = x.GetCustomAttributes(typeof<CommandNameAttribute>, false)
+                if names.Length > 0 then
+                    Some((names.[0] :?> CommandNameAttribute).Name, x)
+                else None)                                        
+        
+    member this.FindCommand bind name =
+        let command = this.Commands |> Seq.tryFind (fun (commandName, _) -> commandName = name)
         let bind = this.Bind bind
-        command |> Option.map (fun x ->
+        command |> Option.map (fun (_,x) ->
             let ctor = x.GetConstructors().[0]
             let parameters = ctor.GetParameters()
             let args : obj array = Array.zeroCreate parameters.Length
             parameters |> Seq.iteri (fun n x -> args.[n] <- bind x)
             ctor.Invoke(args) :?> ITrackerToolsCommand)
 
+    member this.ShowHelp (output:TextWriter) =
+        output.WriteLine "usage is:TrackerTools <command>"
+        this.Commands |> Seq.iter (fun (name, x) -> output.WriteLine(name))
+    
 module Program =
-    let ShowHelp() = ()
-
     let readConfiguration = function
         | "" -> TrackerToolsConfiguration.FromAppConfig()
         | from -> TrackerToolsConfiguration.From(from + ".xml")
 
-    let [<EntryPoint>] main args =
+    let [<EntryPoint>] main args =       
         try
             let commandLineBinder = CommandLineParameterBinder(args)
             let commandName = commandLineBinder.Bind<string>(0)
             let application = ConsoleApplication(readConfiguration(commandLineBinder.GetOption("project")))
             match application.FindCommand (commandLineBinder.Bind) commandName with
             | Some(command) -> command.Invoke()
-            | None -> ShowHelp()
+            | None -> application.ShowHelp Console.Out
         with :? WebException as e ->
             Console.WriteLine e.Message
             use reader = new StreamReader(e.Response.GetResponseStream())
